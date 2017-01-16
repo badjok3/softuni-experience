756083700c9d6a982ec0696febf49a38302f2b4c
badjok3 <dani.popoff@abv.bg>
2017-01-17 00:26:00 +0200

    Fixed the code.

--- brokenCode/brokenCode/Program.cs
+++ brokenCode/brokenCode/Program.cs
@@ -8,19 +8,19 @@ class HolidaysBetweenTwoDates
 
         var startdate = Console.ReadLine();
         var enddate = Console.ReadLine();
-
+        
         var startDate = DateTime.ParseExact(startdate,
-            "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            "d.M.yyyy", CultureInfo.InvariantCulture);
         var endDate = DateTime.ParseExact(enddate,
-            "dd.MM.yyyy", CultureInfo.InvariantCulture);
-
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+ 
         var holidaysCount = 0;
-
-
+ 
         for (var date = startDate; date <= endDate; date.AddDays(1))
         {
             if (date.DayOfWeek == DayOfWeek.Saturday ||
                 date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+            date = date.AddDays(1);
         }
         Console.WriteLine(holidaysCount);
     }
