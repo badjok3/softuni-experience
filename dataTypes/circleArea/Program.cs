f2494f6cecb1015e38601e9850a9be96632e1490
badjok3 <dani.popoff@abv.bg>
2017-01-18 19:44:12 +0200

    Uploaded all exercises.

---
+++ dataTypes/circleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
---
+++ dataTypes/exactSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exactSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumbers = int.Parse(Console.ReadLine());
+
+            decimal result = 0;
+
+            for (int i = 0; i < totalNumbers; i++)
+            {
+                decimal currentNumber = decimal.Parse(Console.ReadLine());
+                result += currentNumber;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
