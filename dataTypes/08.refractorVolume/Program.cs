b7109b9c254e4eec4c237ba605e099a1a81214b2
badjok3 <dani.popoff@abv.bg>
2017-01-19 15:18:34 +0200

    Added two final exercises

---
+++ dataTypes/08.refractorVolume/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.refractorVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = (width*height*length) /3;
+            Console.WriteLine($"Pyramid Volume: {volume:F2}");
+
+        }
+    }
+}
---
+++ dataTypes/09.refractorSpecialNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.refractorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 1; currentNumber <= startingNumber; currentNumber++)
+            {
+                bool isSpecial = false;
+
+
+                int firstDigit = currentNumber % 10;
+                int secondDigit = currentNumber / 10;
+                int total = firstDigit + secondDigit;
+
+                if ((total == 5) || (total == 7) || (total == 11))
+                    isSpecial = true;
+
+                Console.WriteLine($"{currentNumber} -> {isSpecial}");
+
+            }
+
+        }
+    }
+}
