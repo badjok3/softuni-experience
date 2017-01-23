208ce8c82e52f07d6abb7939087134fca3924fcd
badjok3 <dani.popoff@abv.bg>
2017-01-23 14:11:14 +0200

    Updated solution

--- dataTypesExercise/19.Thea the Photographer/Program.cs
+++ dataTypesExercise/19.Thea the Photographer/Program.cs
@@ -4,68 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _19.thePhotographer
+namespace _19.Thea_The_Photographer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int picturesTaken = int.Parse(Console.ReadLine());
-            int secondsToFilter = int.Parse(Console.ReadLine());
-            double goodPicturesPercent = double.Parse(Console.ReadLine()) / 100;
-            int uploadTimePerPicture = int.Parse(Console.ReadLine());
+            var totalPictures = long.Parse(Console.ReadLine());
+            var filterTime = long.Parse(Console.ReadLine());
+            var procentGoodPictures = long.Parse(Console.ReadLine());
+            var uploadTime = long.Parse(Console.ReadLine());
 
-            double filteredPictures = Math.Ceiling(picturesTaken * goodPicturesPercent);
-            double timeForFilter = picturesTaken * secondsToFilter;
-            double totalSeconds = (filteredPictures * uploadTimePerPicture) + timeForFilter;
-            
-            double minutes = 0;
-            while (totalSeconds >= 60)
-            {
-                totalSeconds -= 60;
-                minutes++;
-            }
+            var usefulPictures = (long)Math.Ceiling((totalPictures) * ((double)procentGoodPictures / 100));
 
-            double hours = 0;
-            while (minutes >= 60)
-            {
-                minutes -= 60;
-                hours++;
-            }
+            var filteredSeconds = totalPictures * filterTime;
+            var uploadSeconds = usefulPictures * uploadTime;
 
-            double days = 0;
-            while (hours >= 24)
-            {
-                hours -= 24;
-                days++;
-            }
+            var totalSeconds = filteredSeconds + uploadSeconds;
 
-            Console.Write($"{days}:");
-            if (hours <= 9)
-            {
-                Console.Write($"0{hours}:");
-            }
-            else
-            {
-                Console.Write($"{hours}:");
-            }
-            if (minutes <= 9)
-            {
-                Console.Write($"0{minutes}:");
-            }
-            else
-            {
-                Console.Write($"{minutes}:");
-            }
-            if (totalSeconds <= 9)
-            {
-                Console.Write($"0{totalSeconds}");
-            }
-            else
-            {
-                Console.Write($"{totalSeconds}");
-            }
-
+            Console.WriteLine(TimeSpan.FromSeconds(totalSeconds).ToString(@"d\:hh\:mm\:ss"));
         }
     }
 }
