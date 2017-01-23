b46a65ff6b5133298c9286f492def5f887fcf4ca
badjok3 <dani.popoff@abv.bg>
2017-01-23 22:20:45 +0200

    Added unfinished project

---
+++ Methods And Debugging/08.Multiply Evens By Odds/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.Multiply_Evens_By_Odds
+{
+    using System;
+
+    public class MultiplyEvensByOdds
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static int GetMultipleOfEvensAndOdds(int num)
+        {
+            int sumEvens = GetSumOfEvenDigits(num);
+            int sumOdds = GetSumOfOddDigits(num);
+
+            return sumEvens * sumOdds;
+        }
+
+        public static int GetSumOfEvenDigits(int num)
+        {
+
+        }
+    }
+}
