a39c52768d188a725b2da76d9f6f6a20bc6e25c4
badjok3 <dani.popoff@abv.bg>
2017-01-24 02:40:27 +0200

    Uploaded projects + finished "08.Multiply Evens By Odds"

--- Methods And Debugging/08.Multiply Evens By Odds/Program.cs
+++ Methods And Debugging/08.Multiply Evens By Odds/Program.cs
@@ -6,19 +6,47 @@
     {
         public static void Main(string[] args)
         {
+            int num = int.Parse(Console.ReadLine());
+            var result = GetMultipleOfEvensAndOdds(num);
+
+            Console.WriteLine(result);         
         }
 
         public static int GetMultipleOfEvensAndOdds(int num)
         {
-            int sumEvens = GetSumOfEvenDigits(num);
-            int sumOdds = GetSumOfOddDigits(num);
+            return Math.Abs(GetSumOfEvenDigits(num)
+                  * GetSumOfOddDigits(num));
+        }
 
-            return sumEvens * sumOdds;
+        public static int GetSumOfOddDigits(int num)
+        {
+            var result = 0;
+
+            foreach (var symbol in num.ToString())
+            {
+                var digit = symbol - '0';
+                if (digit % 2 == 1)
+                {
+                    result += digit;
+                }
+            }
+
+            return result;
         }
 
         public static int GetSumOfEvenDigits(int num)
         {
+            var result = 0;
 
+            foreach (var symbol in num.ToString())
+            {
+                var digit = symbol - '0';
+                if (digit % 2 == 0)
+                {
+                    result += digit;
+                }
+            }
+            return result;
         }
     }
 }
---
+++ Methods And Debugging/10.Price Change Alert/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _10.Price_Change_Alert
+{
+    using System;
+
+    public class PriceChangeAlert
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfPrices = int.Parse(Console.ReadLine());
+            double significanceThreshold = double.Parse(Console.ReadLine());
+            double lastPrice = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPrices - 1; i++)
+            {
+                double currentStockPrice = double.Parse(Console.ReadLine());
+                double currentDifference = GetPercentage(lastPrice, currentStockPrice);
+                bool significantDifference = hasSignificantDifference(currentDifference, significanceThreshold);
+                string message = GetDifference(currentStockPrice, lastPrice, currentDifference, significantDifference);
+                Console.WriteLine(message);
+
+                lastPrice = currentStockPrice;
+            }
+        }
+
+        private static string GetDifference(double currentStockPrice, double lastPrice, double difference, bool hasSignificantDifference)
+        {
+            string typeOfChange = string.Empty;
+            if (difference == 0)
+            {
+                typeOfChange = string.Format("NO CHANGE: {0}", currentStockPrice);
+            }
+            else if (!hasSignificantDifference)
+            {
+                typeOfChange = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastPrice, currentStockPrice, difference);
+            }
+            else if (hasSignificantDifference && (difference > 0))
+            {
+                typeOfChange = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastPrice, currentStockPrice, difference);
+            }
+            else if (hasSignificantDifference && (difference < 0))
+            {
+                typeOfChange = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastPrice, currentStockPrice, difference);
+            }
+
+            return typeOfChange;
+        }
+
+        private static bool hasSignificantDifference(double significanceThreshold, double difference)
+        {
+            if (Math.Abs(significanceThreshold + difference) >= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static double GetPercentage(double lastPrice, double currentStockPrice)
+        {
+            double result = (currentStockPrice - lastPrice) / (lastPrice / 100);
+            return result;
+        }
+    }
+}
