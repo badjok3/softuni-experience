e91816eb5d89c170b0b1965eb2811e7163454086
badjok3 <dani.popoff@abv.bg>
2017-01-30 12:47:30 +0200

    Fixed 07.Max Sequence Of Increasing Elements

--- Arrays Exercise/07.Max Sequence Of Increasing Elements/Program.cs
+++ Arrays Exercise/07.Max Sequence Of Increasing Elements/Program.cs
@@ -14,36 +14,33 @@
         public static void MaxSequence(string numbers)
         {
             var parameters = numbers.Split(' ').Select(int.Parse).ToArray();
-            var start = parameters[0];
+            var start = 0;
             var length = 1;
             var bestLength = 0;
-            var bestStart = parameters[0];
+            var bestStart = 0;
             for (int pos = 0; pos < parameters.Length - 1; pos++)
             {
                 if (parameters[pos] < parameters[pos + 1])
                 {
                     length++;
+
+                    if (length > bestLength)
+                    {
+                        bestLength = length;
+                        bestStart = start;
+                    }
                 }
 
                 else
                 {
-                    start = parameters[pos + 1];
+                    start = pos + 1;
                     length = 1;
-                }
-
-                if (length > bestLength)
-                {
-                    bestLength = length;
-                    bestStart = start;
                 }
             }
 
-            var indexOfStart = Array.IndexOf(parameters, bestStart);
-            int[] result = new int[bestLength];
-            for (int i = 0; i < result.Length; i++)
+            for (int i = bestStart; i < bestStart + bestLength; i++)
             {
-                result[i] = parameters[indexOfStart + i];
-                Console.Write($"{result[i]} ");
+                Console.Write($"{parameters[i]} ");
             }
         }
     }
